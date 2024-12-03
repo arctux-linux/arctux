e6e8294b138b1352cfa85b90bfe8f71d4d422722
Valentin Hăloiu <valentin.haloiu@gmail.com>
2024-12-03 20:26:15 +0000

    [ci] Sign packages in `arctux` repository

--- .github/actions/add-pkg-to-repo/entrypoint.sh
+++ .github/actions/add-pkg-to-repo/entrypoint.sh
@@ -10,22 +10,30 @@ main() {
 maybe_configure_rclone() {
     if [[ ! -f "$HOME/.config/rclone/rclone.conf" && -n "$RCLONE_CONFIG_SECRET" ]]; then
         mkdir -p ~/.config/rclone
-        echo "$RCLONE_CONFIG_SECRET" > ~/.config/rclone/rclone.conf
+        echo "$RCLONE_CONFIG_SECRET" >~/.config/rclone/rclone.conf
     fi
 }
 
 generate_sync_file_names() {
     local repo="$1"
-    local pkg="$(basename "$2")"
+    local pkg
+    pkg="$(basename "$2")"
 
-    cat > /tmp/repo-db-files.txt <<EOL
+    cat >/tmp/repo-db-files.txt <<EOL
 ${repo}.db
+${repo}.db.sig
 ${repo}.db.tar.zst
+${repo}.db.tar.zst.sig
 ${repo}.db.tar.zst.old
+${repo}.db.tar.zst.old.sig
 ${repo}.files
+${repo}.files.sig
 ${repo}.files.tar.zst
+${repo}.files.tar.zst.sig
 ${repo}.files.tar.zst.old
+${repo}.files.tar.zst.old.sig
 ${pkg}
+${pkg}.sig
 EOL
 
     echo /tmp/repo-db-files.txt
@@ -42,11 +50,15 @@ add_pkg_to_repo() {
     local local_repo_path="/tmp/repository"
     mkdir "$local_repo_path"
 
+    # Sign package
+    echo -n "$GPG_SIGNING_KEY" | gpg --batch --import
+    gpg --batch --detach-sign --output "${pkg}.sig" --sign "${pkg}"
+
     # TODO: Acquire some kind of lock to avoid possible race conditions between
     # multiple instances of this action running in parallel.
     rclone copy --files-from "$filenames_to_sync" "$bucket" "$local_repo_path"
-    cp "$pkg" "$local_repo_path"
-    repo-add "${local_repo_path}/${repo}.db.tar.zst" "$pkg"
+    cp "$pkg" "$pkg.sig" "$local_repo_path"
+    repo-add --verify --sign "${local_repo_path}/${repo}.db.tar.zst" "$pkg"
     rclone copy --files-from "$filenames_to_sync" --copy-links "$local_repo_path" "$bucket"
     # TODO: Release lock
 }
